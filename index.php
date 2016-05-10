924629ac0b09a99ed3811cb46795eadf8e2d0355
Fabro83 <fabricio.rambaldi@gmail.com>
2016-05-10 15:31:51 +0000

    First commit

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Dependecies -->
+        <link rel="stylesheet" href="bower_components/angular-material/angular-material.min.css">
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/angular/angular.min.js"></script>
+        <script src="bower_components/angular-animate/angular-animate.min.js"></script>
+        <script src="bower_components/angular-aria/angular-aria.min.js"></script>
+        <script src="bower_components/angular-messages/angular-messages.min.js"></script>
+        <script src="bower_components/angular-material/angular-material.min.js"></script>
+        <!-- Custom Scripts -->
+        <script type="text/javascript" src="js/app.js"></script>
+    </head>
+    <body style="height:100%" ng-app="testingApp">
+        <div layout="row" style="height:inherit">
+            <div flex style="background-color:red" ng-controller="acController">
+                <md-autocomplete md-selected-item="selectedItem" 
+                                 md-search-text="searchText" 
+                                 md-items="item in getMatches(searchText)" 
+                                 md-item-text="item.display"
+                                 placeholder="Test Autocomplete" flex>
+                <span md-highlight-text="searchText">{{item.display}}</span>
+                <md-not-found>
+                No matches found.
+                </md-not-found>
+                </md-autocomplete>
+            </div>
+            <div flex style="background-color:blue">
+                Second item in row
+            </div>
+            <div flex>
+                <div flex>First item in column</div>
+                <div flex>Second item in column</div>
+            </div>
+        </div>
+    </body>
+    
+    
+</html>
---
+++ index2.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+ <link rel="stylesheet" href="css/app2.css">
+
+ <body>
+
+
+<div class="page">
+  <header class="site-header">Header</header>
+  <aside class="left-sidebar">
+      <nav class="site-navigation">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Products</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+      </nav>
+   </aside>
+  <section class="main-content"></section>
+  <aside class="right-sidebar">
+     
+     
+     
+     
+    
+
+  <h1>CSS Accordions</h1>
+  <h3>Because we can.</h3>
+  
+  <div class="ac">
+    
+    <input class="ac-input" id="ac-1" name="ac-1" type="checkbox" />
+    <label class="ac-label" for="ac-1">Item 1</label>
+    
+    <article class="ac-text">
+      
+      <div class="ac-sub">
+        <input class="ac-input" id="ac-2" name="ac-2" type="checkbox" />
+        <label class="ac-label" for="ac-2">I love jelly donuts</label>
+        <article class="ac-sub-text"><p>zazaza2.</p></article>
+      </div>
+      
+      <div class="ac-sub">
+        <input class="ac-input" id="ac-3" name="ac-3" type="checkbox" />
+        <label class="ac-label" for="ac-3">They are so delicious</label>
+        <article class="ac-sub-text"><p>Zazazaa.</p></article>
+      </div>
+      
+    </article><!--/ac-text-->
+    
+  </div><!--/ac-->
+  
+  
+  
+  <div class="ac">
+    
+    <input class="ac-input" id="ac-4" name="ac-4" type="checkbox" />
+    <label class="ac-label" for="ac-4">Item 2</label>
+    
+    <article class="ac-text">
+      
+      <div class="ac-sub">
+        <input class="ac-input" id="ac-5" name="ac-5" type="checkbox" />
+        <label class="ac-label" for="ac-5">I also love regular donuts</label>
+        <article class="ac-sub-text"><p>Lalalala2.</p></article>
+      </div>
+      
+      <div class="ac-sub">
+        <input class="ac-input" id="ac-6" name="ac-6" type="checkbox" />
+        <label class="ac-label" for="ac-6">They are also delicious</label>
+        <article class="ac-sub-text"><p>lala3</p></article>
+      </div>
+      
+    </article><!--/ac-text-->
+    
+  </div><!--/ac-->
+
+  
+  </aside>
+  <footer class="site-footer">Footer</footer>
+</div>
+
+
+
+ </body>
+</html>
