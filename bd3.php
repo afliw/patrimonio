bd87d42a4d5b54ea34db5fc82a126ee7c2a10d9f
Fabro83 <fabricio.rambaldi@gmail.com>
2016-05-16 20:05:34 +0000

    Ahora los resultados devueltos por la BD se pasan bien a través de JSON

--- bd3.php
+++ bd3.php
@@ -7,16 +7,16 @@ $username = 'root';
 $password = '';
 
 
-    $dbh = new PDO("mysql:host=$hostname;dbname=patrimonio", $username, $password);
+    $dbh = new PDO("mysql:host=$hostname;dbname=patrimonio", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
-    $sql = "SELECT descripcion FROM PAT_Tipo_bien";
+    $sql = "SELECT descripcion FROM pat_tipo_bien";
 
     $stmt = $dbh->prepare($sql);
 
     $stmt->execute();
 
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    header('Content-Type: application/json');
     echo json_encode($result);
 
 ?>
--- js/class.js
+++ js/class.js
@@ -3,9 +3,11 @@ var app = angular.module("MyApp", []);
 app.controller("GetItems", function($scope, $http) {
     $http.get('bd3.php').
     success(function(data, status, headers, config) {
+        console.log(data);
         $scope.items = data;
     }).
     error(function(data, status, headers, config) {
+        console.log(data);
         // log error
     });
 });
